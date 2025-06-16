3a8cb752e007e6abfe49a8a234da6299d0a00e24
Akanksha Dhyani <akankshadhyani@akankshas-MacBook-Air.local>
2025-06-16 18:28:44 +0530

    codec changed

--- x/params/types/proposal/codec.go
+++ x/params/types/proposal/codec.go
@@ -11,7 +11,7 @@ import (
 // RegisterLegacyAminoCodec registers the necessary concrete types
 // on the provided LegacyAmino codec. These types are used for Amino JSON serialization.
 func RegisterLegacyAminoCodec(cdc *codec.LegacyAmino) {
-	cdc.RegisterConcrete(&MsgUpdateParams{}, "cosmos-sdk/MsgUpdateParams", nil)
+	cdc.RegisterConcrete(&MsgUpdateParams{}, "paramauthority/MsgUpdateParams", nil)
 }
 
 func RegisterInterfaces(registry types.InterfaceRegistry) {
